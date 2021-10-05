20acb456afd928dd6ad5d8a0e277ae957427537f
jorgearturoflores <jorge_flores@cramerpuno.edu.pe>
2021-10-05 02:11:09 -0500

    Se agrega el proyecto

---
+++ js/music-list.js
@@ -0,0 +1,22 @@
+
+let allMusic = [
+  {
+    name: "A Horse with no name",
+    artist: "America",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Zombie",
+    artist: "The Cramberries",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Self Control",
+    artist: "Laura Branigan",
+    img: "music-3",
+    src: "music-3"
+  },
+ 
+];
